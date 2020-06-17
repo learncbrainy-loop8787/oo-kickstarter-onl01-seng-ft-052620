63df1df578b24e0c7c3f8005b3752b859b68dcd1
korey ko <koreykendrick1@gmail.com>
2020-06-17 00:50:48 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,4 +1,11 @@
 class Backer 
-  
-  
- def intitialize(backed)
+  attr_accessor :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    project.backers << 
